56fc996d32c4b50dec11bfa2700854754435ec4d
Egorrrka1613 <reutov622@gmail.com>
2023-08-10 14:54:44 +0400

    fix: add auth cookie

--- networking/requests/1inch.mjs
+++ networking/requests/1inch.mjs
@@ -1,45 +1,38 @@
 import { utils } from '@zomet/sdk';
 
+const inchRoute = 'https://api.1inch.dev';
+
 export const getRoute = (chainId, from, to, amount, gasPrice) => {
-    return new utils.Request(
-        'get',
-        `https://api.1inch.dev/swap/v5.0/${chainId}/quote`,
-        {
-            params: {
-                fromTokenAddress: from,
-                toTokenAddress: to,
-                amount: amount,
-                gasPrice,
-            },
+    return new utils.Request('get', `${inchRoute}/swap/v5.0/${chainId}/quote`, {
+        params: {
+            fromTokenAddress: from,
+            toTokenAddress: to,
+            amount: amount,
+            gasPrice,
         },
-    );
+    });
 };
 
 export const getAllowance = (chainId, tokenAddress, walletAddress) => {
-    return new utils.Request(
-        'get',
-        `https://api.1inch.dev/swap/v5.0/${chainId}/approve/allowance`,
-        {
-            params: {
-                tokenAddress,
-                walletAddress,
-            },
+    return new utils.Request('get', `${inchRoute}/swap/v5.0/${chainId}/approve/allowance`, {
+        params: {
+            tokenAddress,
+            walletAddress,
         },
-    );
+        headers: {
+            Authorization: 'Bearer vBhDqaRSvguBKXgzyrBHEdD3t1oQylVx',
+        },
+    });
 };
 
 export const getSwapTx = (chainId, fromTokenAddress, toTokenAddress, amount, fromAddress, slippage) => {
-    return new utils.Request(
-        'get',
-        `https://api.1inch.dev/swap/v5.0/${chainId}/swap`,
-        {
-            params: {
-                fromTokenAddress,
-                toTokenAddress,
-                amount,
-                fromAddress,
-                slippage,
-            },
+    return new utils.Request('get', `${inchRoute}/swap/v5.0/${chainId}/swap`, {
+        params: {
+            fromTokenAddress,
+            toTokenAddress,
+            amount,
+            fromAddress,
+            slippage,
         },
-    );
+    });
 };
